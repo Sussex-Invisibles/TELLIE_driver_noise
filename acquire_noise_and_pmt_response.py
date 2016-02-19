47c772a1ece1100e5f3142b157bea4a5112016b6
EdLeming <e.leming09@googlemail.com>
2016-02-19 12:12:38 -0500

    Added sweep_noise also debugging folder organisation

--- acquire_noise_and_pmt_response.py
+++ acquire_noise_and_pmt_response.py
@@ -12,7 +12,7 @@ import os
 import sys
 import optparse
 import time
-import sweep
+import sweep_noise
 import scopes
 import scope_connections
 import utils
@@ -23,7 +23,7 @@ if __name__=="__main__":
     parser.add_option("-b",dest="box",help="Box number (1-12)")
     parser.add_option("-c",dest="channel",help="Channel number (1-8)")
     parser.add_option("-i",dest="ipw",help="IPW value to run at")
-    parser.add_option("-n",dest=pulse_number,help="Number of pulses to run over")
+    parser.add_option("-t",dest="nmins",help="Number of mins to run for")
     (options,args) = parser.parse_args()
 
     #Time
@@ -33,39 +33,23 @@ if __name__=="__main__":
     box = int(options.box)
     channel = int(options.channel)
     ipw = int(options.ipw)
-    pulse_number = int(options.pulse_number)
+    nmins = int(options.nmins)
 
     #Fixed parameters
     delay = 1.0 # 1ms -> kHz
     
-    fibre_delay =  0
-    trigger_delay = 0
-
-    # Set up serial connection
-    sc = serial_command.SerialCommand("/dev/tty.usbserial-FTE3C0PG")
-    
-    logical_channel = (box-1)*8 + channel
-    
-    sc.select_channel(logical_channel)
-    sc.set_pulse_width(ipw)
-    sc.set_pulse_height(16383)
-    sc.set_pulse_number(pulse_number)
-    sc.set_pulse_delay(delay)
-    sc.set_fibre_delay(fibre_delay)
-    sc.set_trigger_delay(trigger_delay)
 
     #run the initial setup on the scope
     usb_conn = scope_connections.VisaUSB()
     scope = scopes.Tektronix3000(usb_conn)
     ###########################################
-    scope_channels = [1,3] # We're using channel 1 and 2 (1 for PMT 3 for probe point)!
+    scope_channels = [1,3] # We're using channel 1 and 3 (1 for PMT 3 for probe point)!
     termination = [50,1e6] # Ohms
     trigger_level = 0.5 # half peak minimum
     falling_edge = True
     min_trigger = -0.004
-    y_div_units = 1 # volts
+    y_div_units = [1,0.2] # volts
     x_div_units = 4e-9 # seconds
-    y_offset = -2.5*y_div_units # offset in y (2.5 divisions up)
     x_offset = +2*x_div_units # offset in x (2 divisions to the left)
     record_length = 1e3 # trace is 100e3 samples long
     half_length = record_length / 2 # For selecting region about trigger point
@@ -76,9 +60,10 @@ if __name__=="__main__":
     scope.set_single_acquisition() # Single signal acquisition mode
     scope.set_record_length(record_length)
     scope.set_data_mode(half_length-50, half_length+50)
-    scope.set_edge_trigger(trigger, 1, True) # Rising edge trigger 
+    #scope.set_edge_trigger(trigger, 1, True) # Rising edge trigger 
     
     for i in range(len(scope_channels)):
+	    y_offset = -2.5*y_div_units[i] # offset in y (2.5 divisions up)
 	    scope.set_channel_y(scope_channels[i], y_div_units[i], pos=2.5)
 	    #scope.set_display_y(scope_chan, y_div_units, offset=y_offset)
 	    scope.set_channel_termination(scope_channels[i], termination[i])
@@ -88,15 +73,38 @@ if __name__=="__main__":
 
 
     #Create a new, timestamped, summary file
-    sweep.check_dir('./driver_noise')
-    sweep.check_dir("./pmt_response")
-    sweep.check_dir("./pin_response")
-    saveDirNoise = sweep.check_dir("./driver_noise/Box_%02d/" % (box))
-    saveDirPMTResponse = sweep.check_dir("./driver_noise/Box_%02d/" % (box))
-    saveDirPINResponse = sweep.check_dir("./driver_noise/Box_%02d/" % (box))
+    sweep_noise.check_dir('./driver_noise')
+    sweep_noise.check_dir("./pmt_response")
+    sweep_noise.check_dir("./pin_readings")
+
+    saveDirNoise = sweep_noise.check_dir("./driver_noise/Box_%02d" % (box))
+    saveDirPMTResponse = sweep_noise.check_dir("./pmt_response/Box_%02d" % (box))
+    saveDirPINResponse = sweep_noise.check_dir("./pin_readings/Box_%02d" % (box))
+    
+    saveDirNoise = sweep_noise.check_dir("%s/Channel_%02d" % (saveDirNoise,channel))
+    saveDirPMTResponse = sweep_noise.check_dir("%s/Channel_%02d" % (saveDirPMTResponse,channel))
+    saveDirPINResponse = sweep_noise.check_dir("%s/Channel_%02d" % (saveDirPINResponse,channel))
+
     #Run 1KHz for 10 mins save 100 traces at intervals of 30 secs extract PIN response
-    timestamp = time.strftime("%y%m%d_%H.%M.%S",time.gmtime())
-    output_dirname_noise = "%s/Chan%02d__%s" % (saveDirNoise,channel,timestamp)
-    output_dirname_pmt = "%s/Chan%02d__%s" % (saveDirPMTResponse,channel,timestamp)
-    output_dirname_pin = "%s/Chan%02d__%s" % (saveDirPINResponse,channel,timestamp)
+    start_time = time.time()
+    run_time = 0
+    saveDirs = ["",""]
+    while run_time<60*nmins:
+        run_time = time.time()-start_time
+        #Reading every 30 secs
+        if ((int(run_time))%30)==0:
+            print run_time
+	    timestamp = time.strftime("%y%m%d_%H.%M.%S",time.gmtime())
+	    output_dirname_noise = sweep_noise.check_dir("%s/Time__%s" % (saveDirNoise,timestamp))
+	    output_dirname_pmt = sweep_noise.check_dir("%s/Time__%s" % (saveDirPMTResponse,timestamp))
+	    output_filename_pin = sweep_noise.check_dir("%s/Time__%s.dat" % (saveDirPINResponse,timestamp))
+            saveDirs[0] = output_dirname_pmt
+            saveDirs[1] = output_dirname_noise
+            
+            pin,rms = sweep_noise.sweep_noise(saveDirs,box,channel,ipw,delay,scope)
+            pinFile = fopen(output_filename_pin,"w")
+            pinFile.write("%f %f\n" % (pin,rms))
+            pinFile.close()
+            
+            
    
---
+++ sweep_noise.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python
+#########################
+# sweep_noise.py
+#  Generic module for running
+# IPW sweeps with Tek scope
+# modified to collect a fixed number of traces using scope
+#
+#########################
+import os
+from core import serial_command
+from common import comms_flags
+import math
+import time
+#try:
+import utils
+#except:
+#    pass
+import sys
+import calc_utils as calc
+import numpy as np
+
+port_name = "/dev/tty.usbserial-FTE3C0PG"
+#port_name = "/dev/tty.usbserial-FTGA2OCZ"
+## TODO: better way of getting the scope type
+scope_name = "Tektronix3000"
+_boundary = [0,1.5e-3,3e-3,7e-3,15e-3,30e-3,70e-3,150e-3,300e-3,700e-3,1000]
+_v_div = [1e-3,2e-3,5e-3,10e-3,20e-3,50e-3,100e-3,200e-3,500e-3,1.0]
+_v_div_1 = [1e-3,2e-3,5e-3,10e-3,20e-3,50e-3,100e-3,200e-3,500e-3,1.0,2.0]
+#_v_div = [20e-3, 50e-3, 100e-3, 200e-3, 500e-3, 1] # For scope at sussex
+#_v_div_1 = [20e-3, 50e-3, 100e-3, 200e-3, 500e-3, 1, 2]
+
+sc = None
+sc = serial_command.SerialCommand(port_name)
+
+#initialise sc here, faster options setting
+def start():
+    global sc
+    sc = serial_command.SerialCommand(port_name)
+
+def set_port(port):
+    global port_name
+    port_name = port
+
+def set_scope(scope):
+    global scope_name
+    if scope=="Tektronix3000" or scope=="LeCroy":
+        scope_name = scope
+    else:
+        raise Exception("Unknown scope")
+
+def check_dir(dname):
+    """Check if directory exists, create it if it doesn't"""
+    direc = os.path.dirname(dname)
+    try:
+        os.stat(direc)
+    except:
+        os.mkdir(direc)
+        print "Made directory %s...." % dname
+    return dname    
+
+def return_zero_result():
+    r = {}
+    r['pin'] = 0
+    r['width'], r['width error'] = 0, 0
+    r['rise'], r['rise error'] = 0, 0
+    r['fall'], r['fall error'] = 0, 0
+    r['area'], r['area error'] = 0, 0
+    r['peak'], r['peak error'] = 0, 0
+    return r
+
+def save_scopeTraces(fileName, scope, channel, noPulses):
+    """Save a number of scope traces to file - uses compressed .pkl"""
+    scope._get_preamble(channel)
+    results = utils.PickleFile(fileName, 1)
+    results.add_meta_data("timeform_1", scope.get_timeform(channel))
+
+    #ct = scope.acquire_time_check()
+    #if ct == False:
+    #    print 'No triggers for this data point. Will skip and set data to 0.'
+    #    results.save()
+    #    results.close()
+    #    return False
+
+    t_start, loopStart = time.time(),time.time()
+    for i in range(noPulses):
+        try:
+            ct = scope.acquire_time_check(timeout=.4)
+            results.add_data(scope.get_waveform(channel), 1)
+        except Exception, e:
+            print "Scope died, acquisition lost."
+            print e
+        if i % 100 == 0 and i > 0:
+            print "%d traces collected - This loop took : %1.1f s" % (i, time.time()-loopStart)
+            loopStart = time.time()
+    print "%d traces collected TOTAL - took : %1.1f s" % (i, (time.time()-t_start))
+    results.save()
+    results.close()
+    return True
+
+def find_and_set_scope_y_scale(channel,height,width,delay,scope,scaleGuess=None):
+    """Finds best y_scaling for current pulses
+    """
+    func_time = time.time()
+    sc.fire_continuous()
+    time.sleep(0.1)
+    
+    # If no scale guess, try to find reasonable trigger crossings at each y_scale
+    ct = False
+    if scaleGuess==None:
+        for i, val in enumerate(_v_div):
+            scope.set_channel_y(channel,_v_div[-1*(i+1)], pos=3) # set scale, starting with largest
+            scope.set_edge_trigger( (-1*_v_div[-1*(i+1)]), channel, falling=True)
+            if i==0:
+                time.sleep(1) # Need to wait to clear previous triggered state
+            ct = scope.acquire_time_check(timeout=.5) # Wait for triggered acquisition
+            if ct == True:
+                break
+    else: #Else use the guess
+        if abs(scaleGuess) > 1:
+            guess_v_div = _v_div
+        else:
+            tmp_idx = np.where( np.array(_v_div) >= abs(scaleGuess) )[0][0]
+            guess_v_div = _v_div[0:tmp_idx]
+        for i, val in enumerate(guess_v_div):
+            scope.set_channel_y(channel,guess_v_div[-1*(i+1)],pos=3) # set scale, starting with largest
+            scope.set_edge_trigger( (-1*guess_v_div[-1*(i+1)]), channel, falling=True)
+            if i==0:
+                time.sleep(0.2) # Need to wait to clear previous triggered state
+            ct = scope.acquire_time_check() # Wait for triggered acquisition
+            if ct == True:
+                break
+
+    time.sleep(0.5) # Need to wait for scope to recognise new settings
+    scope._get_preamble(channel)
+    # Calc min value
+    mini, wave = np.zeros( 10 ), None    
+    for i in range( len(mini) ):
+        # Check we get a trigger - even at the lowest setting we might see nothing
+        ct = scope.acquire_time_check(timeout=.4)
+        if ct == False:
+            print 'Triggers missed for this data point. Will skip and set data to 0.'
+            return False
+        wave = scope.get_waveform(channel)
+        mini[i] = min(wave) - np.mean(wave[0:10])
+    min_volt = np.mean(mini)
+    print "MINIMUM MEASUREMENT:", min_volt
+    if np.abs(min_volt) < 0.006:
+        return False
+
+    # Set scope
+    if -1*(min_volt/6) > _v_div_1[-1]:
+        scale = _v_div_1[-1]
+    else: 
+        scale_idx = np.where( np.array(_v_div_1) >= -1*(min_volt/6) )[0][0]
+        scale = _v_div_1[scale_idx]
+    # Because of baseline noise!
+    if scale == 2e-3:
+        trig = -7.5e-3
+        #trig = -4e-3
+    elif scale == 1e-3:
+        trig = -7.5e-3
+        #trig = -3e-3
+    elif scale == 5e-3:
+        trig = -7.5e-3
+    else:
+        trig = -1.*scale
+    print "Preticted scale = %1.3fV, actual scale = %1.3fV, trigger @ %1.4fV" % (-1*(min_volt/6.6) , scale, trig)
+    scope.set_channel_y( channel, scale, pos=3) # set scale, starting with largest
+    scope.set_edge_trigger( trig, channel, falling=True)
+
+    print "TOTAL FUNC TIME = %1.2f s" % (time.time() - func_time)
+    sc.stop()
+    return True
+    
+def sweep_noise(dirs_out,box,channel,width,delay,scope,min_volt=None):
+    """Perform a measurement using a default number of
+    pulses, with user defined width, channel and rate settings.
+    """
+    print '____________________________'
+    print width
+
+    #fixed options
+    height = 16383    
+    fibre_delay = 0
+    trigger_delay = 0
+    pulse_number = 1000
+    #first select the correct channel and provide settings
+    logical_channel = (box-1)*8 + channel
+    
+    sc.select_channel(logical_channel)
+    sc.set_pulse_width(width)
+    sc.set_pulse_height(16383)
+    sc.set_pulse_number(pulse_number)
+    sc.set_pulse_delay(delay)
+    sc.set_fibre_delay(fibre_delay)
+    sc.set_trigger_delay(trigger_delay)
+    
+    # first, run a single acquisition with a forced trigger, effectively to clear the waveform
+    scope._connection.send("trigger:state ready")
+    time.sleep(0.1)
+    scope._connection.send("trigger force")
+    time.sleep(0.1)
+
+    # File system stuff
+    check_dir("%s/" % (dirs_out[0]))
+    check_dir("%s/" % (dirs_out[1]))
+    fname0 = "%sWidth%05d" % (dirs_out[0],width)
+    fname1 = "%sWidth%05d" % (dirs_out[1],width)
+    
+    # Check scope
+    ck = find_and_set_scope_y_scale(1,height,width,delay,scope,scaleGuess=min_volt)
+    if ck == True:
+        print "Saving raw pmt  files to: %s..." % fname0
+        print "Saving raw probe  files to: %s..." % fname1
+        sc.fire_sequence()
+        save_ck0 = save_scopeTraces(fname0, scope, 1, 100)
+        save_ck1 = save_scopeTraces(fname1, scope, 3, 100)
+	#sleeping for 5 seconds to ensure TELLIE has stopped pulsing
+	time.sleep(5)
+    	pin = None
+   	# while not comms_flags.valid_pin(pin,channel):
+    	while pin==None:
+	    pin,rms = sc.tmp_read_rms()
+
+        return pin[logical_channel],rms[logical_channel]
+
+    else:
+        return 0
